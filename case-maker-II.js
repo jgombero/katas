b20b469dcc676e0a9d73ad5c1dcc8872e013aeb1
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-05-19 01:54:44 +0000

    program that changes string into a specified case

--- case-maker-II.js
+++ case-maker-II.js
@@ -1,61 +1,153 @@
-const makeCase = function(input, cases) {
+const makeCase = function(input, style) {
   let output = '';
-  for (let i=0; i<input.length; i++) {
-    if (cases === 'camel') {
+  let styles = [];
+  //stores each style as an array called "styles"
+  if (typeof(style) === 'string') {
+    styles.push(style);
+  } else if (Array.isArray(style) === true) {
+    for (i=0; i<style.length; i++) {
+      styles.push(style[i]);
+    }
+  }
+  // converts input into camelCase
+  const toCamel = function() {
+    for (let i=0; i<input.length; i++) {
       if (input[i-1] === ' ') {
         output += input[i].toUpperCase();
       } else if (input[i] === ' ') {
         output += '';
       } else {
-        output += input[i];
+        output += input[i].toLowerCase();
       }
-    } else if (cases === 'pascal') {
+    }
+    input = output;
+  }
+  // converts input into PascalCase
+  const toPascal = function() {
+    for (let i=0; i<input.length; i++) {
       if (i === 0 || input[i-1] === ' ') {
         output += input[i].toUpperCase();
       } else if (input[i] === ' ') {
         output += '';
       } else {
-        output += input[i];
+        output += input[i].toLowerCase();
       }
-    } else if (cases === 'snake') {
+    }
+    input = output;
+  }
+  // converts input into snake_case
+  const toSnake = function() {
+    for (let i=0; i<input.length; i++) {
       if (input[i] === ' ') {
         output += '_';
       } else {
         output += input[i];
       }
-    } else if (cases === 'kebab') {
+    }
+    input = output;
+  }
+  // converts input into kebab-case
+  const toKebab = function() {
+    for (let i=0; i<input.length; i++) {
       if (input[i] === ' ') {
         output += '-';
       } else {
         output += input[i];
       }
-    } else if (cases === 'title') {
+    }
+    input = output;
+  }
+  // converts input into Title Case
+  const toTitle = function() {
+    for (let i=0; i<input.length; i++) {
       if (input[i-1] === ' ' || i === 0) {
         output += input[i].toUpperCase();
       } else {
         output += input[i];
       }
-    } else if (cases === 'vowel') {
-      if (input[i] === 'a' || input[i] === 'e' || input[i] === 'i' || input[i] === 'o' || input[i] === 'i') {
-        output += input[i].toUpperCase();
-      } else {
-        output += input[i];
-      }
-    } else if (cases === 'consonant') {
-      if (input[i] === 'a' || input[i] === 'e' || input[i] === 'i' || input[i] === 'o' || input[i] === 'i') {
-        output += input[i];
-      } else {
-        output += input[i].toUpperCase();
-      }
-    } else if (cases[0] === 'upper' && cases[1] === 'snake') {
-      if (input[i] === ' ') {
-        output += '_';
-      } else {
-        output += input[i].toUpperCase();
-      }
-    }  
+    }
+    input = output;
   }
-  return output;
+  // converts input into vOwEl
+  const toVowel = function() {
+    for (let i=0; i<input.length; i++) {
+      if (input[i] === 'a' || input[i] === 'e' || input[i] === 'i' || input[i] === 'o' || input[i] === 'i') {
+        output += input[i].toUpperCase();
+      } else {
+        output += input[i];
+      }
+    }
+    input = output;
+  }
+  // converts input into CoNSoNaNT
+  const toConsonant = function() {
+    for (let i=0; i<input.length; i++) {
+      if (input[i] === 'a' || input[i] === 'e' || input[i] === 'i' || input[i] === 'o' || input[i] === 'i') {
+        output += input[i];
+      } else {
+        output += input[i].toUpperCase();
+      }
+    }
+    input = output;
+  }
+  // converts input into UPPERCASE
+  const toUpper = function() {
+    for (let i=0; i<input.length; i++) {
+      output += input[i].toUpperCase();
+    }
+    input = output;
+  }
+  // converts input into lowercase
+  const toLower = function() {
+    for (let i=0; i<input.length; i++) {
+      output += input[i].toLowerCase();
+    }
+    input = output;
+  }
+  // priortizes cases from least to most
+  for (i=0; i<styles.length; i++) {
+    switch (styles[i]) {
+      case 'upper' :
+        toUpper();
+        break;
+      case 'lower' :
+        toLower();
+        break;
+    }
+    output = ''; // resets output for multiple iterations
+  }
+  for (i=0; i<styles.length; i++) {
+    switch (styles[i]) {
+      case 'consonant' :
+        toConsonant();
+        break;
+      case 'vowel' :
+        toVowel();
+        break;
+    }
+    output = '';
+  }
+  for (i=0; i<styles.length; i++) {
+    switch (styles[i]) {
+      case 'title' :
+        toTitle();
+        break;
+      case 'kebab' :
+        toKebab();
+        break;
+      case 'snake' :
+        toSnake();
+        break;
+      case 'pascal' :
+        toPascal();
+        break;
+      case 'camel' :
+        toCamel();
+        break;
+    }
+    output = '';
+  }
+  return input;
 }
 
 console.log(makeCase("this is a string", "camel"));
@@ -66,3 +158,6 @@ console.log(makeCase("this is a string", "title"));
 console.log(makeCase("this is a string", "vowel"));
 console.log(makeCase("this is a string", "consonant"));
 console.log(makeCase("this is a string", ["upper", "snake"]));
+console.log(makeCase("this is a string", ["camel", "upper"]));
+console.log(makeCase("this is a string", ["vowel", "camel"]));
+console.log(makeCase("this is a string", ["vowel", "consonant"]));
